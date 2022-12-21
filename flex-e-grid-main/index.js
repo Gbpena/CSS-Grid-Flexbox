6abf306f16a5add5ff8277bbe138cd601bafd6cf
Gustavo Pena <106341353+Gbpena@users.noreply.github.com>
2022-12-21 12:05:12 -0300

    Delete flex-e-grid-main directory

--- flex-e-grid-main/index.js
+++
@@ -1,6 +0,0 @@
-const botaomenu = document.querySelector('.cabecalho__menu')
-const menu = document.querySelector('.menu-lateral')
-
-botaomenu.addEventListener('click', () => { 
-    menu.classList.toggle('menu-lateral--ativo')
-})
